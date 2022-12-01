c6f754a128f407f4f7d57751ca8dc23a7d846828
JerryX2007 <63323160+JerryX2007@users.noreply.github.com>
2022-11-30 20:37:29 -0500

    Making basic game

--- Apple.java
+++ Apple.java
@@ -1,1 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Creates an apple that falls from the top of the screen to the bottom
+ *
+ * @author Jerry Xing
+ * @version November 2022
+ */
+public class Apple extends Actor
+{
+    /**
+     * Act - do whatever the Apple wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        setLocation(getX(), getY()+1);
+    }
+}
 
--- Elephant.java
+++ Elephant.java
@@ -1,1 +1,34 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
+/**
+ * Elephant for Hungry Elephant game
+ *
+ * @author Jerry Xing
+ * @version 1.0 (updated 28/11/2022)
+ */
+public class Elephant extends Actor
+{
+    GreenfootSound elephantSound = new GreenfootSound("elephantSound.mp3");
+    public void act()
+    {
+        if(Greenfoot.isKeyDown("left")) {
+            move(-5);
+        }
+        else if(Greenfoot.isKeyDown("right")) {
+            move(5);
+        }
+        // Remove apple if elephant eats it
+        eat();
+
+    }
+    /**
+     * Eat the apple and spawn a new apple;
+     */
+    public void eat() {
+        if(isTouching(Apple.class)) {
+            removeTouching(Apple.class);
+            MyWorld world = (MyWorld) getWorld();
+            world.createApple();
+        }
+    }
+}
