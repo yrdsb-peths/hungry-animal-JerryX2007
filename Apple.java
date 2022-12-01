823ef2b1d0da2f630c2293432ce42a4d0f9c4e73
JerryX2007 <63323160+JerryX2007@users.noreply.github.com>
2022-12-01 14:48:48 -0500

    Revert "make apple fall faster"

--- Apple.java
+++ Apple.java
@@ -14,7 +14,7 @@ public class Apple extends Actor
      */
     public void act()
     {
-        setLocation(getX(), getY()+2);
+        setLocation(getX(), getY()+1);
         MyWorld world = (MyWorld) getWorld();
         if(getY() >= world.getHeight()) {
             world.gameOver();
