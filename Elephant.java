0ce869b62b48a602740e8d3d19155abac724841f
JerryX2007 <63323160+JerryX2007@users.noreply.github.com>
2022-12-01 19:00:41 -0500

    Finished tutorial 2

--- Elephant.java
+++ Elephant.java
@@ -9,17 +9,62 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Elephant extends Actor
 {
     GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
+    GreenfootImage[] idleLeft = new GreenfootImage[8];
+    GreenfootImage[] idleRight = new GreenfootImage[8];
+    // Direction the elephant is facing
+    String facing = "right";
+    SimpleTimer animationTimer = new SimpleTimer();
+    /**
+     * Elephant constructor
+     */
+    public Elephant() {
+        for(int i=0;i<8;i++) {
+            idleRight[i] = new GreenfootImage("images/elephant_idle/idle"+i+".png");
+            idleRight[i].scale(100, 100);
+        }
+        
+        for(int i=0;i<idleLeft.length;i++) {
+            idleLeft[i] = new GreenfootImage("images/elephant_idle/idle"+i+".png");
+            idleLeft[i].mirrorHorizontally();
+            idleLeft[i].scale(100, 100);
+        }
+        
+        animationTimer.mark();
+        
+    }
+    
+    /**
+     * Animate the elephant
+     */
+    int imageIndex = 0;
+    public void animateElephant() {
+        if(animationTimer.millisElapsed() < 200) {
+            return;
+        }
+        animationTimer.mark();
+        if(facing.equals("right")) {
+            setImage(idleRight[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleRight.length;
+        }
+        else {
+            setImage(idleLeft[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleLeft.length;
+        }
+    }
     public void act()
     {
         if(Greenfoot.isKeyDown("left")) {
             move(-5);
+            facing = "left";
         }
         else if(Greenfoot.isKeyDown("right")) {
             move(5);
+            facing = "right";
         }
         // Remove apple if elephant eats it
         eat();
-
+        //Animate elephant
+        animateElephant();
     }
     /**
      * Eat the apple and spawn a new apple;
